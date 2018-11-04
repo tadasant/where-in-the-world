b348088492dcb74beb61960e68f5545aa9f561e4
Jeff Appareti <jappareti@gmail.com>
2018-11-04 10:54:18 -0800

    Added google-map-react

--- app/src/components/game/GameScreen.js
+++ app/src/components/game/GameScreen.js
@@ -1,13 +1,22 @@
-import PropTypes from 'prop-types';
-import * as React from 'react';
-import {Component} from 'react';
-import {withRouter} from 'react-router-dom';
 import AnswerSelection from './AnswerSelection';
+import PropTypes from "prop-types";
+import * as React from "react";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import GoogleMapReact from "google-map-react";
 
 class GameScreen extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 0
+  };
   componentDidMount() {
-    const {game} = this.props;
-    const pushToResults = () => this.props.history.push(`/results?gameid=${game.id}`);
+    const { game } = this.props;
+    const pushToResults = () =>
+      this.props.history.push(`/results?gameid=${game.id}`);
     const endDateTime = new Date(game.endDateTime);
     const currentDateTime = new Date();
 
@@ -18,11 +27,15 @@ class GameScreen extends Component {
     }
   }
 
+  handleMapClick = ({x, y, lat, lng, event}) => {
+    console.log(x, y, lat, lng, event);
+  }
+
   render() {
-    const {game} = this.props;
-    return (
-      <div>
-        Game:<br/>
+    const { game } = this.props;
+    return <div>
+        Game:
+        <br />
         <div>
           ID: {game.id}<br/>
           endDateTime: {game.endDateTime}<br/>
@@ -30,9 +43,10 @@ class GameScreen extends Component {
           questionImage: <img src={game.questions.imgURL}/>
           <AnswerSelection questionId={game.questions.id} gameId={game.id}/>
         </div>
-        }
-      </div>
-    )
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact bootstrapURLKeys={{ key: "AIzaSyA8cmyFachXAjlw_lc7QvC8JX1MnmGPJWw" }} defaultCenter={this.props.center} defaultZoom={this.props.zoom} onClick={this.handleMapClick} />
+        </div>
+      </div>;
   }
 }
 
