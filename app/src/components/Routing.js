c11f25a6bd347e433e33762a2c6e4c1c7193b568
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2018-11-04 14:49:27 -0800

    updating last seen correctly

--- app/src/components/Routing.js
+++ app/src/components/Routing.js
@@ -1,18 +1,44 @@
+import gql from 'graphql-tag';
 import * as React from 'react';
-import {Fragment} from 'react';
+import {Fragment, Component} from 'react';
+import {graphql} from 'react-apollo';
 import {BrowserRouter, Route} from 'react-router-dom';
 import GameScreen from './game/GameScreenContainer';
 import LoginScreen from './login/LoginScreen';
 import ResultScreen from './result/ResultScreen';
 
-const Routing = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Route path='/' exact component={LoginScreen}/>
-      <Route path='/game' exact component={GameScreen}/>
-      <Route path='/results' exact component={ResultScreen}/>
-    </Fragment>
-  </BrowserRouter>
-);
+const updatePlayerSeend = gql`
+    mutation UpdatePlayer($id: uuid!, $lastSeenDate: timestamptz!) {
+        update_Player(
+            where: {id: {_eq: $id}},
+            _set: {lastSeen: $lastSeenDate}
+        ) {
+            affected_rows
+        }
+    }
+`;
 
-export default Routing;
+class Routing extends Component {
+  componentDidMount() {
+    if (!this.props.mutate) {
+      console.error('No mutation error code 5879342785');
+    }
+
+    this.props.mutate({ variables: { id: localStorage.getItem('witw-playerId'), lastSeenDate: new Date().toISOString() } })
+
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Fragment>
+          <Route path='/' exact component={LoginScreen}/>
+          <Route path='/game' exact component={GameScreen}/>
+          <Route path='/results' exact component={ResultScreen}/>
+        </Fragment>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default graphql(updatePlayerSeend)(Routing);
