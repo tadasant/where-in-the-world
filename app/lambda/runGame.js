8da5425ee16eed60f97c8fdece6e5bb2c484d724
Tyler Vick <vick.tyler@gmail.com>
2018-11-03 17:12:35 -0700

    Add netlify lambda framework

---
+++ app/lambda/runGame.js
@@ -0,0 +1,27 @@
+function runGame(event, context, callback) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+  };
+
+  const okay = true;
+  const response = {
+    'message': 'Successful lambda test!'
+  }
+
+  if (okay) {
+    callback(null,
+    {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(response),
+    })
+  } else {
+    callback(new Error('Failed lambda test'), {
+      statusCode: 500,
+      headers,
+      body: 'not sure if this does anything',
+    })
+  }
+}
+
+exports.handler = runGame;
